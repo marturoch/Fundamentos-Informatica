4eb81b989c5d32a3d5e6ac715bdcc938df57057f
marturoch <marturoch@gmail.com>
2021-04-15 12:45:09 -0300

    clase 2 y 3
    clase 2 y 3

--- Clase 4/Practicaa/4.py
+++
@@ -1,6 +0,0 @@
-#4. Realizá un programa que encuentre una palabra unida a otra con un guión bajo en un string dado (el string no debe contener espacios).
-import re 
-string = "hola_hola"
-patron = "\w*_\w*"
-print(re.findall(patron, string))
-#NO SALE
--- Clase 4/Teorica/Errores - Desafio 2.py
+++
@@ -1,6 +0,0 @@
-#Desafio 2. Creá una función denominada mitades que tenga como argumento un número e imprima el resultado de la división de ese número por 2
-#Hacemos funcion division y usamos dos parametros
-def division(numero, divisor):
-    print (numero / divisor)
-print(division(4,2))
-print(division(4,0))
